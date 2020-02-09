28b463188eb5a783cac8b27f76d2a739c721cb25
Mary <60063717+yakovlieva811@users.noreply.github.com>
2020-02-09 17:20:42 +0200

    elementary tasks

--- src/main/java/filePraser/Task4.java
+++ src/main/java/filePraser/Task4.java
@@ -1,4 +1,9 @@
 package filePraser;
 
 public class Task4 {
+    public static void main(String[] args){
+
+        FileParser fileParser = new FileParser();
+        fileParser.readFile(args);
+    }
 }
